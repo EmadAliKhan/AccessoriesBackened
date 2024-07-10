5a042ddb63fe574be1fafe1fabf5aae4d24b4489
Emad Ali Khan <142682966+EmadAliKhan@users.noreply.github.com>
2024-07-10 22:46:25 +0500

    Db.js

--- src/db/Db.js
+++ src/db/Db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const connectDB = async()=>{
     try {
-      const connectionInstance = await mongoose.connect("mongodb+srv://EmadKhan:Seemal123@khantodo.6wnfk31.mongodb.net/")
+      const connectionInstance = await mongoose.connect(${process.env.PORT})
         console.log(`mongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("mongooDB connection error..");
